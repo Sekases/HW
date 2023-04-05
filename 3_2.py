3aacc58c5ed10b1006f1185d8082bab1c3a84680
Kanstantsin <103135607+Sekases@users.noreply.github.com>
2023-04-05 12:19:44 +0300

    task 2

---
+++ 3_2.py
@@ -0,0 +1,4 @@
+first, second, third = int(input('Введите первое число: ')), int(input('Введите второе число: ')), int(input('Введите третье число: '))
+
+avg = float((first + second + third) / 3)
+print(round(avg, 3))
