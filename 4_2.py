c7e94c433cdfbfa3f7c8a99b7a3af093599eafe9
Sekases <tipo4ek1213@gmail.com>
2023-04-10 22:28:13 +0200

    task 2(upgrade)

--- 4_2.py
+++ 4_2.py
@@ -1,6 +1,6 @@
 user_text = input('Введите текст: ')
-low_user_text = user_text.lower()
-
+low_user_text_n = user_text.lower()
+low_user_text = sorted(low_user_text_n)
 us_dict = {low_user_text[0] : low_user_text.count(low_user_text[0])}
 
 for i in low_user_text:
