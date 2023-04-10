9127e2d062758af161e027785ab73d6823694e29
Sekases <tipo4ek1213@gmail.com>
2023-04-10 12:05:09 +0200

    task 1

--- 4_1.py
+++ 4_1.py
@@ -1,1 +1,13 @@
-print('Hello world!')
+user_number = int(input('Введите число: '))
+
+list_of_numbers = range(1, user_number + 1)
+q = 1
+
+for i in list_of_numbers:
+    i = 2 ** i
+    print('2 to the power of', q, '=', i)
+    q += 1
+
+
+
+
