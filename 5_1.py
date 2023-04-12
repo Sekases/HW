4af7e5cb927c7490210e5589659fa1439bb0d495
Sekases <tipo4ek1213@gmail.com>
2023-04-12 20:56:44 +0200

    task 5_3.1

--- 5_1.py
+++ 5_1.py
@@ -1,13 +1,15 @@
-num_1 = int(input('Введите первое число(число, больше которого будет вывод): '))
+num_1 = int(input('Введите первое число(число, c которого будет вывод): '))
 num_2 = int(input('Введите второе число(которому будут кратны числа): '))
 num_3 = int(input('Введите третье число(сколько чисел вывести): '))
-itera = -1
+itera = 0
+sch = num_1
 
 while not(itera == num_3):
-    itera += 1
-    if itera <= num_1:
-        continue
-    if not(itera % num_2):
-        print(itera, end= ' ')
+
+    if not(sch % num_2):
+        print(sch, end= ' ')
+        itera += 1
+
+    sch += 1
 
 print('\nEnd!')
