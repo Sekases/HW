253b231e1096ade279d8fb64ed09bb0b163da636
Sekases <tipo4ek1213@gmail.com>
2023-04-26 20:15:35 +0300

    task 7_2

--- 7_1(7_2).py
+++ 7_1(7_2).py
@@ -12,6 +12,28 @@ class Car:
             flag = 'нет'
         return f'Эта машина {self.color}, в ней {self.count_pass_seats} сидения и в ней {flag} детское сидение'
 
+
+
+class Taxi:
+    def __init__(self, cars):
+        self.cars = cars
+
+    def find_car(self, count_pass, is_baby):
+        for car in self.cars:
+            if (car.count_passenger_seats >= count_pass) and (car.is_baby_seat == is_baby) and (not car.is_busy):
+                car.is_busy = True
+                return car
+        return None
+
+
+
+
+
+
+
+
+
+
 kos = Car('красная', 4, True)
 
 print(kos)
